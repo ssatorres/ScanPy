73c4e4f6182bf6a14967d7428fd349673f3db1b1
Igor Oliveira <devigor.tere@gmail.com>
2016-01-21 21:50:56 -0200

    Adicionado o comando para instalar o Uniscan e o Wpscan

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ sudo apt-get install python-pip
 echo "INSTALANDO DEPENDÊNCIAS"
 sudo pip install argparse termcolor subprocess --upgrade
 
+echo "INSTALANDO PROGRAMAS AUXILIARES"
+sudo apt-get install -y wpscan uniscan
+
 sudo chmod +x ScanPy.py
 
