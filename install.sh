1c8c252d5b14755858e258a20c28c477da2d4a78
Igor Oliveira <devigor.tere@gmail.com>
2016-01-22 13:05:51 -0200

    Adicionado o comando para adicionar o repositorio do Kali

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "ADICIONANDO REPO DO KALI"
+apt-key adv --keyserver pgp.mit.edu --recv-keys ED444FF07D8D0BF6
+echo '# Kali linux repositories | Adicionado Por ScanPy\ndeb http://http.kali.org/kali sana main non-free contrib\ndeb http://security.kali.org/kali-security sana/updates main contrib non-free\ndeb http://repo.kali.org/kali kali-bleeding-edge main' >> /etc/apt/sources.list
+
 echo "INSTALANDO O PIP"
 sudo apt-get install python-pip
 
